4056b85f9e9f39b76ba9ffce85e227e2d5303db3
PraneelChugh <86180715+PraneelChugh@users.noreply.github.com>
2022-02-27 12:57:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,11 +18,11 @@ camera = document.getElementById("camera");
 
 console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/GRdtZFIqo/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/t9wn5SZbm/model.json',modelLoaded);
 
 function modelLoaded() {
-    console.log('Model Loaded!');
-  }
+  console.log('Model Loaded!');
+}
 
   function speak(){
       var synth = window.speechSynthesis;
@@ -33,5 +33,45 @@ function modelLoaded() {
   }
 
   function check(){
-      
+      img = document.getElementById('captured_image')
+      classifier.classify(img, gotResults)
+  }
+
+  function gotResults(error, results) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(results);
+      document.getElementById('result_Gesture_name').innerHTML=results[0].label;
+      document.getElementById('result_Gesture_name2').innerHTML=results[1].label;
+      prediction_1 = results[0].label;
+      prediction_2 = results[1].label;
+      speak();
+  
+      if(results[0].label == "Victory")
+      {
+        document.getElementById("updated_gestures").innerHTML = "&#9996";
+      }
+      if(results[0].label == "Amazing")
+      {
+        document.getElementById("updated_gestures").innerHTML = "&#128076";
+      }
+      if(results[0].label == "Best")
+      {
+        document.getElementById("updated_gestures").innerHTML = "&#128077";
+      }
+  
+      if(results[1].label == "Victory")
+      {
+        document.getElementById("updated_gestures2").innerHTML = "&#9996";
+      }
+      if(results[1].label == "Amazing")
+      {
+        document.getElementById("updated_gestures2").innerHTML = "&#128076";
+      }
+      if(results[1].label == "Best")
+      {
+        document.getElementById("updated_gestures2").innerHTML = "&#128077";
+      }
+    }
   }
